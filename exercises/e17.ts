ad51061758b6d73d3d173716f96fb8afa3773718
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-12-10 12:47:42 -0700

    Pass all e16-e18 tests

--- exercises/e17.ts
+++ exercises/e17.ts
@@ -1,3 +1,35 @@
-export const minBy = (array, cb) => {};
+export const minBy = (
+  array: Element[],
+  cb: (element: Element | undefined) => {}
+) => {
+  if (array.length === 0) return undefined;
 
-export function maxBy(array, cb) {}
+  let min = cb(array[0]);
+  let minElement = array[0];
+
+  for (let element of array) {
+    if (cb(element) < min) {
+      min = cb(element);
+      minElement = element;
+    }
+  }
+  return minElement;
+};
+
+export function maxBy(
+  array: Element[],
+  cb: (element: Element | undefined) => {}
+) {
+  if (array.length === 0) return undefined;
+
+  let max = cb(array[0]);
+  let maxElement = array[0];
+
+  for (let element of array) {
+    if (cb(element) > max) {
+      max = cb(element);
+      maxElement = element;
+    }
+  }
+  return maxElement;
+}
--- exercises/e18.ts
+++ exercises/e18.ts
@@ -2,8 +2,28 @@
 // Return the year with the greatest number of Asteroids discoveries
 // Return example: 1902
 // Hint: use a Map data structure instead of an object if you want typescript to be happy
+import { Asteroid } from "../data/data";
 
-export function getGreatestDiscoveryYear(asteroids) {}
+export function getGreatestDiscoveryYear(asteroids: Asteroid[]) {
+  const years = new Map<number, number>();
+  for (let asteroid of asteroids) {
+    const year = asteroid.discoveryYear;
+    if (years.has(year)) {
+      years.set(year, years.get(year)! + 1);
+    } else {
+      years.set(year, 1);
+    }
+  }
+  let maxYear = 0;
+  let maxYearCount = 0;
+  for (let [year, count] of years) {
+    if (count > maxYearCount) {
+      maxYear = year;
+      maxYearCount = count;
+    }
+  }
+  return maxYear;
+}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-16"
