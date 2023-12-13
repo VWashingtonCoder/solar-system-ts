41ae54513da819f0f453d838d13230eef927665d
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-12-13 10:55:04 -0700

    Update to pass e16 type check

--- exercises/e16.ts
+++ exercises/e16.ts
@@ -1,9 +1,9 @@
 // see e16.md
 
 // DO NOT USE `any` for this, you will need to use generics in order to pass both the vitest tests and the tsc compiler
-export function find(
-  array: [],
-  callback: (element: boolean | number | string | [] | {}) => {}
+export function find<X>(
+  array: X[],
+  callback: (element: X) => {}
 ) {
   for (let element of array) {
     if (callback(element)) {
